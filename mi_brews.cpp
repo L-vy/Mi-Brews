eb95183451a0fbf123b81d8f448fae00c94d8090
L-vy <latasha.khoe@binus.ac.id>
2024-12-26 15:49:14 +0700

    Add files via upload

--- mi_brews.cpp
+++ mi_brews.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
+#include <time.h>
 
 #define MAX_ITEMS 15
 #define MAX_TOPPINGS 4
 #define MAX_ORDERS 50
+
+#define HISTORY_FILE "order_history.txt"
+
 
 // Struktur data untuk menu utama minuman
 typedef struct {
@@ -15,25 +18,25 @@ typedef struct {
 
 // Daftar menu minuman
 Item items[MAX_ITEMS] = {
-	// Minuman Berkafein
+    // Minuman Berkafein
     {"Midnight Espresso", 15000}, 
-	{"Caramel Cloud Macchiato", 20000}, 
-	{"Hazelnut Latte", 18000},
+    {"Caramel Cloud Macchiato", 20000}, 
+    {"Hazelnut Latte", 18000},
     {"Mocha Java", 17000}, 
-	{"Nitro Cold Brew", 22000}, 
-	
-	// Minuman non-kafein
-	{"Tropical Twist Tea", 15000},
+    {"Nitro Cold Brew", 22000}, 
+
+    // Minuman non-kafein
+    {"Tropical Twist Tea", 15000},
     {"Berry Bliss Smoothie", 20000}, 
-	{"Matcha Magic", 22000}, 
-	{"Ginger Elixir", 18000},
-	
-	// Minuman Spesial
+    {"Matcha Magic", 22000}, 
+    {"Ginger Elixir", 18000},
+
+    // Minuman Spesial
     {"Coconut Dream", 15000}, 
-	{"Unicorn Latte", 25000}, 
-	{"Galaxy Smoothie", 25000},
+    {"Unicorn Latte", 25000}, 
+    {"Galaxy Smoothie", 25000},
     {"Cloud 9", 18000}, 
-	{"Ocean Breeze", 20000}
+    {"Ocean Breeze", 20000}
 };
 
 // Struktur data untuk topping
@@ -44,100 +47,82 @@ typedef struct {
 
 // Daftar menu topping
 addOn toppings[MAX_TOPPINGS] = {
-    {"Grass Jelly", 5000}, {"Pearl", 5000}, {"Pudding", 5000}, {"Coconut Jelly", 5000}
+    {"Grass Jelly", 3500}, 
+    {"Pearl", 4500}, 
+    {"Pudding", 5000}, 
+    {"Coconut Jelly", 5000}
 };
 
 // Variabel global untuk menyimpan data pesanan
-int counter = 0;						// Jumlah pesanan
-int quantities[MAX_ITEMS] = {0}; 		// Kuantitas setiap minuman   
-int itemTotals[MAX_ITEMS] = {0};		// Total harga untuk setiap minuman
-int orderIndices[MAX_ORDERS] = {0};		// Indeks item dalam daftar pesanan
+int total = 0;                            // Harga total pesanan
+int counter = 0;                          // Jumlah pesanan
+float disc = 0;                           // Jumlah diskon yang didapatkan
+int loyaltyPoints = 0;                    // Banyak loyalty points
 
-// Functions
+int quantities[MAX_ITEMS] = {0};          // Kuantitas setiap minuman   
+int itemTotals[MAX_ITEMS] = {0};          // Total harga untuk setiap minuman
+int orderIndices[MAX_ORDERS] = {0};       // Indeks item dalam daftar pesanan
+
+
+// Deklarasi Fungsi
 void welcome();
-void displayMenu(Item items[], int nItems, addOn toppings[], int nToppings);
-int addTopping(addOn toppings[], int nToppings);
+void displayMenu(Item items[], addOn toppings[]);
+int addTopping(addOn toppings[]);
 
-void orderItem(int *total);
-void updateOrder(int *total);
-void deleteOrder(int *total);
+void orderItem();
+void updateOrder();
+void deleteOrder();
 
 void previewBill();
-void bill(int total);
+float dateDiscount();
+void bill();
+
+void processOrder();
+void viewTransactionHistory();
+void saveOrderHistory();
+void freeDrink();
+
 
 int main() {
-    int nItems = 14, nToppings = 4, total = 0;
-    char addMore;
-	
-	// Menampilakan halaman awal dan menu
-	welcome();	
-    displayMenu(items, nItems, toppings, nToppings);
-	
-	// Memproses pemesanan
-    do {
-        orderItem(&total);
-        printf("Apakah ingin memesan lagi? (y/n): ");
-        scanf("%c", &addMore); getchar();
-    } while (addMore == 'y');
-
-    system("cls");
-    previewBill();
-
-    int option;
-    while (true) {
-    	puts("");
-    	// Navigasi jika ingin memperbahrui pesanan atau langsung pembayaran
-        puts("============ Navigation ============");
-        puts("1. Add Item(s)");
-        puts("2. Update Item(s)");
-        puts("3. Delete Item(s)");
-        puts("4. Finalize Bill");
+    welcome();            // Menampilkan halaman awal
+    while (true) {        // Menampilkan halaman menu
+        int menu;
+        
+        puts("");
+        puts("============ Menu ============");
+        puts("1. Start Ordering");
+        puts("2. View History");
+        puts("3. Redeem Free Drink");
+        puts("   *if eligible");
+        puts("4. Exit");
         printf(">> ");
-        scanf("%d", &option); getchar();
-
-        switch (option) {
+        scanf("%d", &menu); getchar();
+        
+        switch(menu) {
             case 1:
-            	system("cls");
-            	puts("Adding order........");
-				displayMenu(items, nItems, toppings, nToppings);
-                orderItem(&total);
-                puts("");
-                
-                system("cls");
-                previewBill();
+                processOrder();
+                saveOrderHistory();
                 continue;
             case 2:
-            	puts("Updating order........");
-                updateOrder(&total);
+                viewTransactionHistory();
                 continue;
             case 3:
-            	puts("Deleting order........");
-                deleteOrder(&total);
+                freeDrink();
                 continue;
             case 4:
-                bill(total);
                 break;
-            default:
+            default: 
+                puts("Pilihan ini tidak valid, silakan coba lagi.");
                 break;
         }
         break;
-    }
-
-    // Pelanggan mendapat 1 poin untuk setiap Rp 10.000
-    // Untuk setiap 10 poin yang dimiliki, pelanggan mendapatkan minuman gratis (Cloud 9)
-    int pointsEarned = total / 10000;
-    puts("\n");
-    printf("!! Anda mendapatkan poin sebanyak: %d poin(s) !!\n", pointsEarned);
-	
-    if (pointsEarned >= 10) {
-        puts("Selamat! Anda mendapatkan minuman gratis : Cloud 9!");
     }
 
     puts("\nTerima kasih telah memesan di Mi Brews!");
     return 0;
 }
 
-// Menampilkan halaman selamat datang
+// Menampilkan halaman awal
 void welcome(){
 	puts(" __       __  __        _______ ");                                             
 	puts("|  \\     /  \\|  \\      |       \\");                                           
@@ -159,15 +144,14 @@ void welcome(){
 	puts("      `---'                                  `---'                                                                    ");
 
 
-	printf("\n");
-	printf("Enter any key to start ordering...");
-	char key = getch();
+	printf("\nEnter any key to start ordering...");
+	getchar();
 	system("cls");
 }
 
 // Menampilkan menu minuman dan topping
-void displayMenu(Item items[], int nItems, addOn toppings[], int nToppings) {
-    printf("=================== Menu Minuman ==================\n");
+void displayMenu(Item items[], addOn toppings[]) {
+    printf("\n=================== Menu Minuman ==================\n");
     printf("\n\t--------- Berkafein ---------\n");
     for (int i = 0; i < 5; i++) {
         printf("%d. %-30s \tRp %d\n", i + 1, items[i].name, items[i].price);
@@ -179,24 +163,23 @@ void displayMenu(Item items[], int nItems, addOn toppings[], int nToppings) {
     }
 
     printf("\n\t------- Minuman Spesial -------\n");
-    for (int i = 10; i < nItems; i++) {
+    for (int i = 10; i < 14; i++) {
         printf("%d. %-30s \tRp %d\n", i + 1, items[i].name, items[i].price);
     }
 
     printf("\n================== Menu Toppings ==================\n\n");
-    for (int i = 0; i < nToppings; i++) {
+    for (int i = 0; i < 4; i++) {
         printf("%d. %-30s \tRp %d\n", i + 1, toppings[i].name, toppings[i].price);
     }
     printf("\n");
 }
 
 // Menambahkan topping ke pesanan
-int addTopping(addOn toppings[], int nToppings) {
+int addTopping(addOn toppings[]) {
     int toppingChoice, toppingTotal = 0;
     char addTop;
     do {
-        puts("");
-        printf("Apakah Anda ingin menambahkan topping? (y/n): ");
+        printf("\nApakah Anda ingin menambahkan topping? (y/n): ");
         scanf("%c", &addTop); getchar();
 
         if (addTop == 'y') {
@@ -205,7 +188,7 @@ int addTopping(addOn toppings[], int nToppings) {
                 printf(">> ");
                 scanf("%d", &toppingChoice); getchar();
 
-                if (toppingChoice < 1 || toppingChoice > nToppings) {
+                if (toppingChoice < 1 || toppingChoice > 4) {
                     printf("Pilihan topping tidak valid. Coba lagi.\n");
                     continue;
                 }
@@ -224,53 +207,67 @@ int addTopping(addOn toppings[], int nToppings) {
 }
 
 // Membuat pesanan baru
-void orderItem(int *total) {
-    int choice, quantity;
-    do {
-        puts("");
-        puts("Silakan pilih nomor minuman yang diinginkan ^^");
-        printf(">> ");
-        scanf("%d", &choice); getchar();
-
-        if (choice < 1 || choice > 14) {
-            puts("\n!! Pilihan ini tidak valid. Silakan coba lagi. !!");
-            continue;
-        }
-    } while (choice < 1 || choice > 14);
-
-    do {
-        puts("");
-        printf("Berapa jumlah minuman %s?\n", items[choice - 1].name);
-        printf(">> ");
-        scanf("%d", &quantity); getchar();
-
-        if (quantity < 1) {
-            puts("\n!! Minimal minuman yang dibeli adalah 1. Silakan coba lagi. !!");
-            continue;
-        }
-    } while (quantity < 1);
-
-    int itemTotal = items[choice - 1].price * quantity;
-    int toppingTotal = addTopping(toppings, MAX_TOPPINGS);
-
-    itemTotal += toppingTotal;
-    *total += itemTotal;
-
-    quantities[choice - 1] += quantity; 
-    itemTotals[choice - 1] += itemTotal;
-    orderIndices[counter++] = choice - 1;
-
-    printf("Pesanan %s dengan total Rp %d berhasil ditambahkan.\n\n", items[choice - 1].name, itemTotal);
+void orderItem() {
+	displayMenu(items, toppings);
+	char addMore;
+	do{
+		int choice, quantity;
+	    do {
+	        puts("\nSilakan pilih nomor minuman yang diinginkan^^");
+	        printf(">> ");
+	        scanf("%d", &choice); getchar();
+	
+	        if (choice < 1 || choice > 14) {
+	            puts("\n!! Pilihan ini tidak valid. Silakan coba lagi. !!");
+	            continue;
+	        }
+	    } while (choice < 1 || choice > 14);
+	
+	    do {
+	        printf("\nBerapa jumlah minuman %s?\n", items[choice - 1].name);
+	        printf(">> ");
+	        scanf("%d", &quantity); getchar();
+	
+	        if (quantity < 1) {
+	            puts("\n!! Minimal minuman yang dibeli adalah 1. Silakan coba lagi. !!");
+	            continue;
+	        }
+	    } while (quantity < 1);
+	
+	    int itemTotal = items[choice - 1].price * quantity;
+	    int toppingTotal = addTopping(toppings);
+	
+	    itemTotal += toppingTotal;
+	    total += itemTotal;
+	
+	    quantities[choice - 1] += quantity; 
+	    itemTotals[choice - 1] += itemTotal;
+	    orderIndices[counter++] = choice - 1;
+	    
+	    // Melihat jika minuman yang di pesan merupakan minuman spesial (yang ada di index 10-14)
+	    // Mendapatkan 1 poin untuk setiap minuman spesial yang dipesan. 
+	    if (choice >= 10) {
+	        loyaltyPoints += quantity; 
+	        printf("Anda telah mendapatkan %d poin loyalitas!\n", quantity);
+	    }
+	
+	    printf("Pesanan %s dengan total Rp %d berhasil ditambahkan.\n\n", items[choice - 1].name, itemTotal);	
+	    
+	    printf("Apakah ingin memesan lagi? (y/n): ");
+		scanf("%c", &addMore); getchar();
+	} while (addMore == 'y');
+	
+    system("cls");
+	previewBill();
 }
 
 // Memperbarui jumlah minuman untuk pesanan yang sudah ada 
-void updateOrder(int *total) {
+void updateOrder() {
     system("cls");
     previewBill();
     
     int num;
-    puts("");
-    puts("Pilihlah nomor order untuk memperbaharui item");
+    puts("\nPilihlah nomor order untuk memperbaharui item");
     printf(">> ");
     scanf("%d", &num); getchar();
 
@@ -280,12 +277,11 @@ void updateOrder(int *total) {
     }
 
     int currentIndex = orderIndices[num - 1];
-    *total -= itemTotals[currentIndex];
+    total -= itemTotals[currentIndex];
 
     int quantity;
     do {
-        puts("");
-        printf("Berapa jumlah minuman %s?\n", items[currentIndex].name);
+        printf("\nBerapa jumlah minuman %s?\n", items[currentIndex].name);
         printf(">> ");
         scanf("%d", &quantity); getchar();
 
@@ -296,10 +292,10 @@ void updateOrder(int *total) {
     } while (quantity < 1);
 
     int itemTotal = items[currentIndex].price * quantity;
-    int toppingTotal = addTopping(toppings, MAX_TOPPINGS);
+    int toppingTotal = addTopping(toppings);
 
     itemTotal += toppingTotal;
-    *total += itemTotal;
+    total += itemTotal;
 
     quantities[currentIndex] = quantity;
     itemTotals[currentIndex] = itemTotal;
@@ -310,19 +306,19 @@ void updateOrder(int *total) {
     previewBill();
 }
 
-// Menghapus pesanan
-void deleteOrder(int *total) {
+// Menghapus salah satu pesanan
+void deleteOrder() {
     system("cls");
-    puts("\t\t============================ Order List ============================");
+    puts("\n\t\t============================ Order List ============================");
     for (int i = 0; i < counter; i++) {
         int index = orderIndices[i];
         printf("Order#%d : %-30s\t | Qty: %d\t | Price: Rp %d\t | Total: Rp %d\n",
                i + 1, items[index].name, quantities[index], items[index].price, itemTotals[index]);
     }
+    
 	int num;
 	do{
-	    puts("");
-	    puts("Pilihlan nomor order untuk menghapus item");
+	    puts("\nPilihlan nomor order untuk menghapus item");
 	    printf(">> ");
 	    scanf("%d", &num); getchar();
 	
@@ -332,9 +328,8 @@ void deleteOrder(int *total) {
 	    }
 	} while (num < 1 || num > counter);
     
-
     int deleteIndex = orderIndices[num - 1];
-    *total -= itemTotals[deleteIndex];
+    total -= itemTotals[deleteIndex];
     quantities[deleteIndex] = 0;
     itemTotals[deleteIndex] = 0;
 
@@ -349,7 +344,7 @@ void deleteOrder(int *total) {
 
 // Menampilkan bill sebelum ditetapkan
 void previewBill() {
-    puts("\t\t============================ Preview Bill ============================");
+    puts("\n\t\t============================ Preview Bill ============================");
     for (int i = 0; i < counter; i++) {
         int index = orderIndices[i];
         printf("Order#%d : %-30s\t | Qty: %d\t | Price: Rp %d\t | Total: Rp %d\n",
@@ -357,20 +352,41 @@ void previewBill() {
     }
 }
 
+// Mendapatkan diskon ketika tanggal hari dan bulan sama
+// contoh : 11 November atau 12 Desember
+float dateDiscount() {
+    time_t t;
+    time(&t);
+    struct tm *timeInfo = localtime(&t);
+
+    int year = timeInfo->tm_year + 1900; 
+    int month = timeInfo->tm_mon + 1;   
+    int day = timeInfo->tm_mday;       
+
+    // Jika hari dan bulan memiliki tanggal yang sama, 
+    // Pelanggan mendapat diskon sebesar 22%
+    if (month == day) {
+        puts("\nAnda mendapatkan diskon 22%!");
+        return disc += 0.22; 
+    } 
+
+    return disc; 
+}
+
 // Menampilkan bill akhir dan melakukan pembayaran
-void bill(int total) {
+void bill() {
     system("cls");
     previewBill();
     
     typedef struct {
     	char name[30];
 	} payment;
+	
     payment method[3] = {{"QRIS"}, {"Debit Card"}, {"Cash"}};
     
     int pay;
     do{
-    	puts("");
-    	puts("============ Payment Method ============");
+    	puts("\n============ Payment Method ============");
     	puts("1. QRIS");
     	puts("2. Debit Card");
     	puts("3. Cash");
@@ -378,7 +394,7 @@ void bill(int total) {
     	scanf("%d", &pay); getchar();
 	} while (pay < 1 || pay > 3);
 	
-	float disc;
+	// Mendapatkan diskon untuk pembayaran tertentu
 	switch (pay){
 		case 1:
 			disc = 0.1;
@@ -392,7 +408,9 @@ void bill(int total) {
 			break;
 	}
     
-    char key = getch();
+    disc = dateDiscount();
+    
+	getchar();
     system("cls");
     puts("\t\t============================ Rincian Pesanan ============================");
     for (int i = 0; i < counter; i++) {
@@ -410,3 +428,129 @@ void bill(int total) {
     printf("%-18s : \tRp %d\n", "Discount", discount);
     printf("%-18s : \tRp %d\n", "Total biaya", (total-discount));
 }
+
+// Untuk memesan minuman 
+void processOrder(){
+	system("cls");
+	orderItem();
+
+	while (true){
+		int option;
+		// Navigasi jika ingin memperbahrui pesanan atau langsung pembayaran
+	    puts("\n============ Navigation ============");
+		puts("1. Add Item(s)");
+	    puts("2. Update Item(s)");
+	    puts("3. Delete Item(s)");
+		puts("4. Finalize Bill");
+		printf(">> ");
+		scanf("%d", &option); getchar();
+				
+		switch (option) {
+			case 1:
+				system("cls");
+				puts("Adding items . . .");
+				orderItem();
+				continue;
+			case 2:
+			    updateOrder();
+			    continue;
+			case 3:
+			    deleteOrder();
+				continue;
+			case 4:
+			    bill();
+			    
+			    printf("\nApakah Anda ingin memesan lagi? (y/n): ");
+			    char again;
+				scanf(" %c", &again); getchar();
+					                
+			    if (again == 'y' || again == 'Y') {
+				    system("cls");
+				    continue;
+				} else break;
+			default:
+				puts("Pilihan ini tidak valid, silakan coba lagi.");
+			break;
+		}
+	    break;
+    }
+    
+    system("cls");
+}
+
+// Untuk menyimpan order history dalam "order_history.txt"
+void saveOrderHistory() {
+    FILE *file = fopen(HISTORY_FILE, "a");
+    if (file != NULL) {
+        fprintf(file, "======================== Order History ========================\n");
+        for (int i = 0; i < counter; i++) {
+            int index = orderIndices[i];
+            int pointsEarned = (index >= 10) ? quantities[index] : 0;
+            fprintf(file, "Order ID: %d\n", i + 1);
+            fprintf(file, "\tItem: %s | ", items[index].name);
+            fprintf(file, " Quantity: %d |", quantities[index]);
+            fprintf(file, " Price: Rp %d\n", items[index].price);
+            
+            fprintf(file, "\tTotal Price: Rp %d\n", itemTotals[index]);
+            fprintf(file, "\tLoyalty Points Earned: %d\n", pointsEarned);
+            fprintf(file, "--------------------------------------------------------------\n");
+        }
+        fprintf(file, "===============================================================\n");
+        fclose(file);
+    }
+}
+
+// Untuk melihat order history di console
+void viewTransactionHistory() {
+	system("cls");
+    FILE *file = fopen(HISTORY_FILE, "r");
+    if (file == NULL) {
+        puts("Tidak ada riwayat transaksi ditemukan.");
+        return;
+    }
+
+    char line[200];
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+    fclose(file);
+    
+    getchar();
+    system("cls");
+}
+
+// Mendapatkan minuman gratis (Minuman Spesial) untuk setiap 10 poin
+void freeDrink() { 
+	system("cls");
+	
+    if (loyaltyPoints >= 10) { 
+        int freeChoice; 
+        puts("\nSelamat! Anda memiliki cukup poin untuk mendapatkan 1 minuman gratis."); 
+        puts("Pilih 1 minuman gratis dari menu:"); 
+        puts("*tanpa topping");
+        
+        printf("\n\t------- Minuman Spesial -------\n");
+        for (int i = 10; i < 15; i++) {
+            printf("%d. %-30s \tRp %d\n", i + 1, items[i].name, items[i].price);
+        }
+
+        do { 
+            printf("Masukkan nomor minuman yang ingin Anda pilih: "); 
+            scanf("%d", &freeChoice); getchar(); 
+
+            if (freeChoice < 11 || freeChoice > 15) { 
+                printf("Pilihan tidak valid. Silakan coba lagi.\n"); 
+                continue; 
+            } 
+            printf("Minuman %s gratis berhasil ditambahkan ke pesanan Anda.\n", items[freeChoice - 1].name); 
+            loyaltyPoints -= 10; 
+            break; 
+        } while (1); 
+    } 
+	else { 
+        printf("Poin loyalitas Anda belum cukup untuk mendapatkan minuman gratis.\n"); 
+    } 
+    
+    getchar();
+    system("cls");
+} 
